a72e2ef51a205653ec02be1d93b99dbdb24fca87
Che Sergiu <37384588+CHESERGIU@users.noreply.github.com>
2020-07-03 10:56:18 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace App17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputData = Console.ReadLine();
+            double first = Convert.ToSingle(inputData);
+            inputData = Console.ReadLine();
+            double second = Convert.ToSingle(inputData);
+            double price = second < 14 ? (first / 2) : first;
+            Console.WriteLine(price);
+        }
+    }
+}
